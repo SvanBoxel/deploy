fee1fa34079210d27952c1b0013f2190660f6ba1
Sebass van Boxel <svboxel@gmail.com>
2020-02-20 11:00:08 +0100

    use uuid

--- dist/index.js
+++ dist/index.js
@@ -31720,7 +31720,9 @@ const event = process.env.GITHUB_EVENT_NAME;
 const payloadPath = process.env.GITHUB_EVENT_PATH;
 const payload = require(path.resolve(payloadPath));
 core.debug(`Receiving event ${JSON.stringify(event)}`);
-probot.receive({ name: event, payload }).catch(err => core.setFailed(`Action failed with error: ${err.message}`))
+console.log(2)
+probot.receive({ name: event, payload, id: '14c12d16-bcd0-42f6-853f-4eabeb484087' }).catch(err => core.setFailed(`Action failed with error: ${err.message}`))
+console.log(3)
 
 /***/ }),
 /* 310 */
