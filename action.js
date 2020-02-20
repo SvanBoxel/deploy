aeb6bfc43f8e7c357bf5e1924ca6263ef21a5e65
Sebass van Boxel <svboxel@gmail.com>
2020-02-20 11:52:12 +0100

    experiment with setting status

--- action.js
+++ action.js
@@ -3,17 +3,29 @@ process.env.DISABLE_STATS = 'true';
 const handler = require('./index')
 const path = require('path');
 const core = require('@actions/core');
+const github = require('@actions/github');
 const { createProbot } = require('probot');
 
 // Setup Probot app
-const githubToken = process.env.GITHUB_TOKEN;
-const probot = createProbot({ githubToken });
+const token = process.env.GITHUB_TOKEN;
+const octokit = new github.GitHub(token);
+const context = github.context;
+const probot = createProbot({ token });
 probot.setup([handler]);
+
 
 // Process the event
 const event = process.env.GITHUB_EVENT_NAME;
 const payloadPath = process.env.GITHUB_EVENT_PATH;
 const payload = require(path.resolve(payloadPath));
 core.debug(`Receiving event ${JSON.stringify(event)}`);
+
+octokit.checks.update({
+  owner: context.repo.owner.login,
+  repo: context.repo.name,
+  check_run_id: process.env.GITHUB_RUN_ID,
+  conclusion: 'success'
+})
+
 probot.receive({ name: event, payload }).catch(err => core.setFailed(`Action failed with error: ${err.message}`))
   
