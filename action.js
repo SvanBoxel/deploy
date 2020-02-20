1846c950b7a1f271bdf56a7adcef7f0b132ab47f
Sebass van Boxel <svboxel@gmail.com>
2020-02-20 11:52:12 +0100

    experiment with setting status

--- action.js
+++ action.js
@@ -2,18 +2,23 @@ process.env.DISABLE_STATS = 'true';
 
 const handler = require('./index')
 const path = require('path');
-const core = require('@actions/core');
 const { createProbot } = require('probot');
+const core = require('@actions/core');
+
+const token = process.env.GITHUB_TOKEN;
 
 // Setup Probot app
-const githubToken = process.env.GITHUB_TOKEN;
-const probot = createProbot({ githubToken });
+const probot = createProbot({ token });
 probot.setup([handler]);
 
 // Process the event
-const event = process.env.GITHUB_EVENT_NAME;
-const payloadPath = process.env.GITHUB_EVENT_PATH;
-const payload = require(path.resolve(payloadPath));
-core.debug(`Receiving event ${JSON.stringify(event)}`);
-probot.receive({ name: event, payload }).catch(err => core.setFailed(`Action failed with error: ${err.message}`))
-  
+async function run() {
+  const event = process.env.GITHUB_EVENT_NAME;
+  const payloadPath = process.env.GITHUB_EVENT_PATH;
+  const payload = require(path.resolve(payloadPath));
+  core.debug(`Receiving event ${JSON.stringify(event)}`);
+
+  probot.receive({ name: event, payload }).catch(err => core.setFailed(`Action failed with error: ${err.message}`))
+}
+
+run();
