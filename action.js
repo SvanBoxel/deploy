46948c32cbe850fbf5f5ae138e1b49933eaa992c
Sebass van Boxel <svboxel@gmail.com>
2020-02-20 11:52:12 +0100

    experiment with setting status

--- action.js
+++ action.js
@@ -2,18 +2,33 @@ process.env.DISABLE_STATS = 'true';
 
 const handler = require('./index')
 const path = require('path');
-const core = require('@actions/core');
 const { createProbot } = require('probot');
+const { Octokit } = require("@octokit/rest");
+
+const token = process.env.GITHUB_TOKEN;
 
 // Setup Probot app
-const githubToken = process.env.GITHUB_TOKEN;
-const probot = createProbot({ githubToken });
+const probot = createProbot({ token });
 probot.setup([handler]);
 
+// Setup octokit
+const octokit = new Octokit({ auth: token });
+
 // Process the event
-const event = process.env.GITHUB_EVENT_NAME;
-const payloadPath = process.env.GITHUB_EVENT_PATH;
-const payload = require(path.resolve(payloadPath));
-core.debug(`Receiving event ${JSON.stringify(event)}`);
-probot.receive({ name: event, payload }).catch(err => core.setFailed(`Action failed with error: ${err.message}`))
-  
+async function run() {
+  const event = process.env.GITHUB_EVENT_NAME;
+  const payloadPath = process.env.GITHUB_EVENT_PATH;
+  const payload = require(path.resolve(payloadPath));
+  core.debug(`Receiving event ${JSON.stringify(event)}`);
+
+  octokit.checks.update({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    check_run_id: process.env.GITHUB_RUN_ID,
+    conclusion: 'success'
+  })
+
+  probot.receive({ name: event, payload }).catch(err => core.setFailed(`Action failed with error: ${err.message}`))
+}
+
+run();
